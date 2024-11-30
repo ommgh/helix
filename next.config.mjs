2b200b594c425e1ce89eec1b2c3d759ee607331c
Om Mishra <om.works01@gmail.com>
2024-11-30 19:37:20 +0530

    next--config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  reactStrictMode: false,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
 
 export default nextConfig;
